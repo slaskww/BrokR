705677ec5ee8c349ec66ce0f02656b8fd6630b6d
lukasz <slaskww@gmail.com>
2019-09-07 21:02:26 +0200

    Added basic packages

---
+++ src/main/java/com/apl/brokr/BrokrApplication.java
@@ -0,0 +1,13 @@
+package com.apl.brokr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apl/brokr/BrokrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apl.brokr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrokrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
