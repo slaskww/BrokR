d7ffe08ef0fe2fb4fb8a07ab963fd4432ae65d62
lukasz <slaskww@gmail.com>
2019-09-09 19:35:09 +0200

    Added services for model objects, added import.sql file with test data

---
+++ src/main/java/com/apl/brokr/services/ClientService.java
@@ -0,0 +1,16 @@
+package com.apl.brokr.services;
+
+import com.apl.brokr.model.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+}
