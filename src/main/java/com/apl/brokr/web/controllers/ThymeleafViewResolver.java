f698eb9b738f45e7ec540db2d6f5fa6b716e6a6d
Pawel Malkowski <pawel_malkowski@wp.pl>
2019-09-10 01:05:41 +0200

    Add templates, add thymeleaf application properties

---
+++ src/main/java/com/apl/brokr/web/controllers/ThymeleafViewResolver.java
@@ -0,0 +1,29 @@
+package com.apl.brokr.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
+
+}
